062c86860bd36d64e8cc38d395e92891ee6df36d
Anita <anitaukah2@gmail.com>
2025-03-05 20:49:28 +0100

    made an adjustment in the submenu of the sidebar

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 /** @format */
+// const showMenu = (toggleId) => {
+// 	const toggle = document.getElementById(toggleId);
+// 	nav = document.getElementById("navId")
+
+// 	toggle.addEventListener("click", () => {
+// 		nav.classList.toggle('show-menu');
+// 		toggle.classList.toggle("show-icon");
+// 	});
+// };
+// showMenu('nav-toggle', 'nav-menu');
+// nav-menu
+
+function showSidebar() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.style.display = "block";
+}
+
+function hideSidebar() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.style.display = "none";
+}
